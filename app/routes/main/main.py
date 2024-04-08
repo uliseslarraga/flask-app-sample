8bc8e3694e84c837f3d8e77a23e73a9abf9ec033
Ulises Lárraga <ulises.larraga@wizeline.com>
2024-04-07 22:11:26 -0600

    feat(aws): add s3 endpoint

--- app/routes/main/main.py
+++ app/routes/main/main.py
@@ -1,6 +1,9 @@
+import logging
+import boto3
 from flask import Flask, render_template
 from app.routes.main import main_routes
 from config import basedir
+from app.object_wrapper import ObjectWrapper
 
 @main_routes.route('/')
 def index():
@@ -9,3 +12,13 @@ def index():
 @main_routes.route('/healthcheck')
 def health():
     return 'Ok'
+
+@main_routes.route('/s3')
+def s3():
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+    s3_resource = boto3.resource("s3")
+    bucket = s3_resource.Bucket("three-tier-storage-app-development")
+
+    listed_files = ObjectWrapper.list(bucket, "Screenshot")
+    return f"Their keys are: {', '.join(l.key for l in listed_files)}"
