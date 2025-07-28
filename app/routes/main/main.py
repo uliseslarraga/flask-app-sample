a08fafd8d792533c46fa70c285a463aec14692e5
Cruz Ulises Larraga Ramirez <uliseslarraga@gmail.com>
2025-07-28 13:43:17 -0600

    feat: Update bucket name

--- app/routes/main/main.py
+++ app/routes/main/main.py
@@ -18,7 +18,7 @@ def s3():
     logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
     s3_resource = boto3.resource("s3")
-    bucket = s3_resource.Bucket("three-tier-storage-app-development")
+    bucket = s3_resource.Bucket("tofu-multi-env-storage-app-development")
 
     listed_files = ObjectWrapper.list(bucket, "Screenshot")
     return f"Their keys are: {', '.join(l.key for l in listed_files)}"
